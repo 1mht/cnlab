ccb59fc0131965cbdff5f5dc87a049ba7be28329
1mht <derechin1@163.com>
2025-11-30 18:31:09 +0800

    finish:ASS4

--- assignment4/http_proxy.go
+++ assignment4/http_proxy.go
@@ -1,8 +1,95 @@
 /*****************************************************************************
- * http_proxy.go                                                                 
- * Names: 
+ * http_proxy.go
+ * Names:
  * NetIds:
  *****************************************************************************/
 
- // TODO: implement an HTTP proxy
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+)
+ 
+ func handleConnection(conn net.Conn) {
+	 defer conn.Close()
+	 // 读取客户端请求
+	 req, err := http.ReadRequest(bufio.NewReader(conn))
+	 if err != nil {
+		 log.Println("Error reading request:", err)
+		 return
+	 }
+ 
+	 // 只处理GET请求
+	 if req.Method != "GET" {
+		 resp := fmt.Sprintf("HTTP/1.1 500 Internal Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n")
+		 conn.Write([]byte(resp))
+		 return
+	 }
+ 
+	 // 创建一个新的请求发送到远程服务器，默认端口号80
+	 url := req.URL
+	 remoteAddr := strings.Split(url.Host, ":")[0]
+	 remotePort := "80"
+	 if len(strings.Split(url.Host, ":")) > 1 {
+		 remotePort = strings.Split(url.Host, ":")[1]
+	 }
+	 remoteConn, err := net.Dial("tcp", net.JoinHostPort(remoteAddr, remotePort))
+	 if err != nil {
+		 resp := fmt.Sprintf("HTTP/1.1 500 Internal Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n")
+		 conn.Write([]byte(resp))
+		 return
+	 }
+	 defer remoteConn.Close()
+ 
+	 // 修改请求头以确保关闭连接
+	 req.Header.Set("Connection", "close")
+	 req.RequestURI = ""
+	 req.URL.Scheme = ""
+	 req.URL.Host = ""
+ 
+	 // 发送请求到远程服务器
+	 if err := req.Write(remoteConn); err != nil {
+		 resp := fmt.Sprintf("HTTP/1.1 500 Internal Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n")
+		 conn.Write([]byte(resp))
+		 return
+	 }
+ 
+	 // 从远程服务器读取响应并转发给客户端 
+	 // 使用1024字节缓冲区循环读取服务器响应并原样转发给客户端
+	 buf := make([]byte, 1024)
+	 for {
+		 n, err := remoteConn.Read(buf)
+		 if err != nil {
+			 break
+		 }
+		 if _, err := conn.Write(buf[:n]); err != nil {
+			 break
+		 }
+	 }
+ }
+ 
+ func main() {
+	 port := os.Args[1] // 从命令行参数获取端口号
+	 listener, err := net.Listen("tcp", ":"+port)
+	 if err != nil {
+		 log.Fatal("Failed to start listening on port ", port, " error: ", err)
+	 }
+	 defer listener.Close()
+	 log.Println("Listening on port", port)
+ 
+	 for {
+		 conn, err := listener.Accept()
+		 if err != nil {
+			 log.Println("Error accepting connection:", err)
+			 continue
+		 }
+		 go handleConnection(conn)
+	 }
+ }
  
--- assignment4/http_proxy_DNS.go
+++ assignment4/http_proxy_DNS.go
@@ -1,6 +1,6 @@
 /*****************************************************************************
- * http_proxy_DNS.go                                                                 
- * Names: 
+ * http_proxy_DNS.go
+ * Names:
  * NetIds:
  *****************************************************************************/
 
@@ -10,3 +10,130 @@
 // with the name http_proxy_DNS.go, thus overwriting this file, then edit it
 // to add DNS prefetching (don't forget to change the filename in the header
 // to http_proxy_DNS.go in the copy of http_proxy.go)
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	req, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		log.Println("Error reading request:", err)
+		return
+	}
+
+	if req.Method != "GET" {
+		resp := fmt.Sprintf("HTTP/1.1 500 Internal Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n")
+		conn.Write([]byte(resp))
+		return
+	}
+
+	// 创建一个新的请求
+	url := req.URL
+	remoteAddr := strings.Split(url.Host, ":")[0]
+	remotePort := "80"
+	if len(strings.Split(url.Host, ":")) > 1 {
+		remotePort = strings.Split(url.Host, ":")[1]
+	}
+	remoteConn, err := net.Dial("tcp", net.JoinHostPort(remoteAddr, remotePort))
+	if err != nil {
+		resp := fmt.Sprintf("HTTP/1.1 500 Internal Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n")
+		conn.Write([]byte(resp))
+		return
+	}
+	defer remoteConn.Close()
+
+	// 关闭连接
+	req.Header.Set("Connection", "close")
+	req.RequestURI = ""
+	req.URL.Scheme = ""
+	req.URL.Host = ""
+
+	// 发送请求并接受response
+	if err := req.Write(remoteConn); err != nil {
+		resp := fmt.Sprintf("HTTP/1.1 500 Internal Error\r\nContent-Length: 0\r\nConnection: close\r\n\r\n")
+		conn.Write([]byte(resp))
+		return
+	}
+
+	buf := make([]byte, 1024)
+    var response []byte
+    for {
+        n, err := remoteConn.Read(buf)
+        if err != nil {
+            break
+        }
+        response = append(response, buf[:n]...)
+        if _, err := conn.Write(buf[:n]); err != nil {
+            break
+        }
+    }
+
+    go DNSCache(response)
+}
+
+func DNSCache(htmlDocument []byte) {
+	doc, err := html.Parse(strings.NewReader(string(htmlDocument)))
+
+	if err != nil {
+		log.Println("Broken html document!")
+		return
+	}
+
+	// 深搜遍历
+	var dfs func(*html.Node)
+	dfs = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, a := range node.Attr {
+				// 有href的a标签
+				if a.Key == "href" && strings.HasPrefix(a.Val, "http") {
+					log.Println("href=",a.Val)
+					host := strings.Split(a.Val, "/")[2] // 获取链接地址（删掉http头）
+					go func(host string) {
+						result, err := net.LookupHost(host)
+						if err != nil {
+							log.Println("DNS lookup failed")
+						} else{
+							log.Println("DNS prefetched: ", host)
+							log.Println("Result: ",result)
+						}
+					}(host)
+				}
+			}
+		}
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			dfs(c)
+		}
+	}
+	dfs(doc)
+}
+
+func main() {
+	port := os.Args[1] // 从命令行参数获取端口号
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal("Failed to start listening on port ", port, " error: ", err)
+	}
+	defer listener.Close()
+	log.Println("Listening on port", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
